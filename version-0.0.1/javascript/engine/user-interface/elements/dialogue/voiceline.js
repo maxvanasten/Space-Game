24d9bd5f5fc685d526d8e0d6970957c0a21125df
Max van Asten <maxvasten@gmail.com>
2024-12-31 18:01:38 +0100

    Restructured repo for future

--- version-0.0.1/javascript/engine/user-interface/elements/dialogue/voiceline.js
+++
@@ -1,7 +0,0 @@
-class VoiceLine {
-    constructor(position, text, delay) {
-        this.position = position; //left or right
-        this.text = text; //string
-        this.delay = delay;
-    }
-}
--- version-0.0.1/javascript/planet/planethandler.js
+++
@@ -1,9 +0,0 @@
-class PlanetHandler {
-    constructor() {
-        this.types = ["gas_planet", "guild_planet"];
-    }
-
-    getType() {
-        return this.types[Math.floor(Math.random() * this.types.length)];
-    }
-}
